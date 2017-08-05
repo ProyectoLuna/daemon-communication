f24d1aba441d06490a39c9b3e700116b3d06dae6
Miguel Garcia <mgm.mgm.85@gmail.com>
2017-08-05 15:55:19 +0200

    Added empty class

---
+++ inc/DaemonComm.h
@@ -0,0 +1,17 @@
+/*
+ * DaemonComm.h
+ *
+ *  Created on: Aug 5, 2017
+ *      Author: migarna
+ */
+
+#ifndef DAEMONCOMM_H_
+#define DAEMONCOMM_H_
+
+class DaemonComm {
+public:
+	DaemonComm();
+	virtual ~DaemonComm();
+};
+
+#endif /* DAEMONCOMM_H_ */
---
+++ src/DaemonComm.cpp
@@ -0,0 +1,18 @@
+/*
+ * DaemonComm.cpp
+ *
+ *  Created on: Aug 5, 2017
+ *      Author: migarna
+ */
+
+#include "DaemonComm.h"
+
+DaemonComm::DaemonComm() {
+	// TODO Auto-generated constructor stub
+
+}
+
+DaemonComm::~DaemonComm() {
+	// TODO Auto-generated destructor stub
+}
+
